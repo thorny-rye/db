a4228692ce5b98daa5d39a7149174a5ebc7df8dc
thorny-rye <k.mytarev@bk.ru>
2024-11-16 15:08:59 +0300

    Add files via upload

---
+++ module_14_1.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connection = sqlite3.connect('not_telegram.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+#for i in range(1, 11):
+#    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
+#                   (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
+
+#for i in range(1, 11, 2):
+#    cursor.execute('UPDATE Users SET balance = 500 WHERE id = ? ', (f'{i}'))
+
+
+#for i in range(1, 11, 3):
+#    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}', ))
+
+cursor.execute('SELECT * FROM Users ')
+users = cursor.fetchall()
+
+for user in users:
+    print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
+connection.commit()
+connection.close()
