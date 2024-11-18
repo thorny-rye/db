f5a97c0251edeee44f02ff9075fc97c51b6155d8
thorny-rye <k.mytarev@bk.ru>
2024-11-18 23:24:31 +0300

    Update module_14_1.py

--- module_14_1.py
+++ module_14_1.py
@@ -13,6 +13,8 @@ balance INTEGER NOT NULL
 )
 ''')
 
+cursor.execute('DELETE FROM Users ')
+
 for i in range(1, 11):
     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)',
                    (f'User{i}', f'example{i}@gmail.com', i * 10, 1000))
@@ -24,10 +26,12 @@ for i in range(1, 11, 2):
 for i in range(1, 11, 3):
     cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}', ))
 
+connection.commit()
+
 cursor.execute('SELECT * FROM Users WHERE age != 60 ')
 users = cursor.fetchall()
 
+connection.close()
+
 for user in users:
     print(f'Имя: {user[1]} | Почта: {user[2]} | Возраст: {user[3]} | Баланс: {user[4]}')
-connection.commit()
-connection.close()
